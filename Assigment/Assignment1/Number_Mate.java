793eacfbb47ef60b24f4495f1af75aabf2bd67fb
ibnvakbar <r.ibnuakbar@gmail.com>
2017-10-11 23:46:10 +0700

    Add Assignment1

---
+++ Assigment/Assignment1/Number_Mate.java
@@ -0,0 +1,15 @@
+package Assignment1;
+
+/**
+ * Created by rama on 10/11/17.
+ */
+public class Number_Mate {
+    String Name;
+    Integer life_point;
+    Integer score;
+    Integer room;
+
+
+
+
+}
