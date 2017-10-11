3f362bf6417beab413a44227a45357b1eb02ea55
ibnvakbar <r.ibnuakbar@gmail.com>
2017-10-12 00:08:03 +0700

    TODO: Create class PlayTheGame

---
+++ Assigment/Assignment1/Assigment1Main.java
@@ -0,0 +1,21 @@
+package Assignment1;
+
+import java.util.Scanner;
+
+/**
+ * Created by rama on 10/11/17.
+ */
+public class Assigment1Main{
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Selamat Datang di Permainan Tebak Bilik");
+        System.out.print("Silahkan Masukan Nama Anda: ");
+        String name = input.nextLine();
+        Integer life_point = 40;
+        Number_Mate nm = new Number_Mate(name,life_point);
+        System.out.println("Pemain: "+nm.getName());
+        System.out.println("Life Point: "+nm.getLife_point());
+
+
+    }
+}
--- Assigment/Assignment1/Number_Mate.java
+++ Assigment/Assignment1/Number_Mate.java
@@ -1,5 +1,7 @@
 package Assignment1;
 
+import com.sun.xml.internal.bind.v2.TODO;
+
 /**
  * Created by rama on 10/11/17.
  */
@@ -9,7 +11,31 @@ public class Number_Mate {
     Integer score;
     Integer room;
 
+    public Number_Mate(String Name, Integer life_point){
+        this.Name=Name;
+        this.life_point=life_point;
+    }
 
+    public String getName() {
+        return Name;
+    }
 
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Integer getLife_point() {
+        return life_point;
+    }
+
+    public void setLife_point(Integer life_point) {
+        this.life_point = life_point;
+    }
+
+//    TODO
+
+    public static void PlayTheGame(){
+
+    }
 
 }
