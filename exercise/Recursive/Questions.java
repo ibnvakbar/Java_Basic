07cb4d8b238acf8b2115f521fe944aa59a30734e
ibnvakbar <r.ibnuakbar@gmail.com>
2017-11-04 12:10:31 +0700

    lab6

--- exercise/Recursive/Questions.java
+++ exercise/Recursive/Questions.java
@@ -6,23 +6,35 @@ package Recursive;
 public class Questions {
     public static void main(String[]args){
         Questions q  = new Questions();
-        System.out.println(q.banyak("haloooo"));
+        System.out.println(q.banyak("hhloooo"));
+        System.out.println(q.endX("rama"));
     }
 
 //    1
     public int banyak(String a){
         int counter = 0;
+//        System.out.println(a.charAt(0));
+//        System.out.println(a.charAt(1));
         if(a.length()<=1){
             return counter;
         }else if(a.charAt(0)==a.charAt(1)){
             counter ++;
             banyak(a.substring(1,a.length()));
-//        }else{
-//            counter +=0;
-//            banyak(a.substring(1,a.length()));
-//        }
+        }else{
+            counter +=0;
+            banyak(a.substring(1,a.length()));
+        }
         return counter;
     }
 
-
+    public String endX(String param){
+        if(param.length()==0){
+            return "";
+        }else if(param.length()==1) {
+            return param + "X";
+        }else if(param.length()>1){
+            return endX(param.substring(1,param.length()));
+        }
+        return param+"X";
+    }
 }
