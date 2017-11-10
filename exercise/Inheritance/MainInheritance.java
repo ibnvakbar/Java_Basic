802ce2f06930e9cf057fe55f85069de26b21ae8e
ibnvakbar <r.ibnuakbar@gmail.com>
2017-11-11 06:56:05 +0700

    new exercise-Inherintace

---
+++ exercise/Inheritance/MainInheritance.java
@@ -0,0 +1,12 @@
+package Inheritance;
+
+/**
+ * Created by rama on 11/11/17.
+ */
+public class MainInheritance {
+    public static void main(String[]args){
+        Mobil m = new Mobil("500",(double)450000, (double) 3000000, "Merah");
+        System.out.println("Harga Bekas: "+m.getHargaBekas());
+        Truk t = new Truk();
+    }
+}
--- exercise/Inheritance/Truk.java
+++ exercise/Inheritance/Truk.java
@@ -5,9 +5,16 @@ package Inheritance;
  */
 public class Truk extends Mobil{
 
+//    public Truk(String kecepatan, Double hargaBaru, Double hargaBekas, String warna) {
+//        super(kecepatan, hargaBaru, hargaBekas, warna);
+//    }
 
-    public Truk(String kecepatan, Double hargaBaru, Double hargaBekas, String warna) {
-        super(kecepatan, hargaBaru, hargaBekas, warna);
+    public Double getHargaBekas(Integer berat){
+        if(berat > 2000){
+            return super.getHargaBekas()*0.01;
+        }else{
+            return super.getHargaBekas()*0.02;
+        }
     }
 
 
