9c95dc6cef2c53751a865451e708171e43a4f4f7
Ibnu Akbar <r.ibnuakbar@gmail.com>
2017-11-21 07:35:26 +0700

    Create MakhlukHidup.java

---
+++ MakhlukHidup.java
@@ -0,0 +1,15 @@
+public class MakhlukHidup{
+	public String nama;
+
+	public MakhlukHidup(){
+		this.nama=nama;
+	}
+
+	public String setNama(String nama){
+		this.nama=nama;
+	}
+
+	public static Boolean isBreath(){
+		return true;
+	}
+}
